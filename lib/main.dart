aebc0d8ad47b0cacb0ff0abe43f89d235d47af3c
Nipun Tharuka Fernando <nipunjayanga@gmail.com>
2024-10-14 08:20:26 -0400

    added main navigation pages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'dart:core';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+
 // TODO: Add localstore package functionality
 // import 'package:localstore/localstore.dart';
 
@@ -23,16 +27,17 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'My Recipe Book',
+        title: 'Recipe Book AI',
         theme: ThemeData(
           primarySwatch: Colors.yellow,
         ),
         home: Scaffold(
+          floatingActionButton: const FloatingActionBuilder(),
           appBar: AppBar(
             backgroundColor: Colors.yellow[900],
             title: Center(
               child: Text(
-                'MY RECIPE BOOK',
+                'RECIPE BOOK AI',
                 style: GoogleFonts.lato(
                   textStyle: Theme.of(context).textTheme.displayLarge,
                   fontSize: 36,
@@ -49,6 +54,88 @@ class _MyAppState extends State<MyApp> {
             }),
           ),
         ));
+  }
+}
+
+class FloatingActionBuilder extends StatelessWidget {
+  const FloatingActionBuilder({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SpeedDial(
+      animatedIcon: AnimatedIcons.menu_close,
+      children: [
+        SpeedDialChild(
+          shape: const CircleBorder(),
+          child: const Icon(Icons.edit),
+          label: 'Add Recipe Manually',
+          backgroundColor: const Color.fromARGB(255, 236, 64, 207),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const AddRecipeScreen(),
+              ),
+            );
+          },
+        ),
+        SpeedDialChild(
+          shape: const CircleBorder(),
+          child: const Icon(Icons.camera_alt),
+          label: 'Add Recipe with AI',
+          backgroundColor: const Color.fromARGB(255, 236, 64, 213),
+          onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const AddRecipeScreen(),
+              ),
+            );
+          },
+        ),
+      ],
+      backgroundColor: Colors.pink[400],
+      shape: const CircleBorder(),
+      child: const Icon(Icons.add),
+    );
+  }
+}
+
+class AddRecipeScreen extends StatelessWidget {
+  const AddRecipeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Add Recipe'),
+      ),
+      body: const Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Add Recipe',
+              style: const TextStyle(
+                color: Colors.black38,
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Add Recipe Description',
+              style: const TextStyle(
+                color: Colors.black38,
+                fontSize: 18,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
 
@@ -94,6 +181,11 @@ class RecipleListTile extends StatelessWidget {
   }
 }
 
+List<String> titles = <String>[
+  'Prep',
+  'Cook',
+];
+
 class RecipeDetailScreen extends StatelessWidget {
   final int index;
   const RecipeDetailScreen({
@@ -103,29 +195,139 @@ class RecipeDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Item $index'),
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
+    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
+    const int tabsCount = 2;
+    return DefaultTabController(
+      initialIndex: 1,
+      length: tabsCount,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Recipe Title'),
+          notificationPredicate: (ScrollNotification notification) {
+            return notification.depth == 1;
+          },
+          scrolledUnderElevation: 4.0,
+          shadowColor: Theme.of(context).shadowColor,
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: const Icon(Icons.shopping_cart),
+                text: titles[0],
+              ),
+              Tab(
+                icon: const Icon(Icons.soup_kitchen),
+                text: titles[1],
+              ),
+            ],
+          ),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Item $index',
+                style: const TextStyle(
+                  color: Colors.black38,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Description of Item $index',
+                style: const TextStyle(
+                  color: Colors.black38,
+                  fontSize: 18,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    );
+  }
+}
+
+class AppBarExample extends StatelessWidget {
+  const AppBarExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
+    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
+    const int tabsCount = 3;
+
+    return DefaultTabController(
+      initialIndex: 1,
+      length: tabsCount,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('AppBar Sample'),
+          // This check specifies which nested Scrollable's scroll notification
+          // should be listened to.
+          //
+          // When `ThemeData.useMaterial3` is true and scroll view has
+          // scrolled underneath the app bar, this updates the app bar
+          // background color and elevation.
+          //
+          // This sets `notification.depth == 1` to listen to the scroll
+          // notification from the nested `ListView.builder`.
+          notificationPredicate: (ScrollNotification notification) {
+            return notification.depth == 1;
+          },
+          // The elevation value of the app bar when scroll view has
+          // scrolled underneath the app bar.
+          scrolledUnderElevation: 4.0,
+          shadowColor: Theme.of(context).shadowColor,
+          bottom: TabBar(
+            tabs: <Widget>[
+              Tab(
+                icon: const Icon(Icons.cloud_outlined),
+                text: titles[0],
+              ),
+              Tab(
+                icon: const Icon(Icons.beach_access_sharp),
+                text: titles[1],
+              ),
+              Tab(
+                icon: const Icon(Icons.brightness_5_sharp),
+                text: titles[2],
+              ),
+            ],
+          ),
+        ),
+        body: TabBarView(
           children: <Widget>[
-            Text(
-              'Item $index',
-              style: TextStyle(
-                color: Colors.black38,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
+            ListView.builder(
+              itemCount: 25,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  tileColor: index.isOdd ? oddItemColor : evenItemColor,
+                  title: Text('${titles[0]} $index'),
+                );
+              },
             ),
-            Text(
-              'Description of Item $index',
-              style: TextStyle(
-                color: Colors.black38,
-                fontSize: 18,
-                fontWeight: FontWeight.normal,
-              ),
+            ListView.builder(
+              itemCount: 25,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  tileColor: index.isOdd ? oddItemColor : evenItemColor,
+                  title: Text('${titles[1]} $index'),
+                );
+              },
+            ),
+            ListView.builder(
+              itemCount: 25,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  tileColor: index.isOdd ? oddItemColor : evenItemColor,
+                  title: Text('${titles[2]} $index'),
+                );
+              },
             ),
           ],
         ),
