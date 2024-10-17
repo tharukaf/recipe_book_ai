ef9447c04d03d7fcfcee0806007bf849b1c2e5ba
Nipun Tharuka Fernando <nipunjayanga@gmail.com>
2024-10-17 05:24:37 -0400

    added prep window items and add item alert dialog

--- lib/widgets/RecipeBookAppBar.dart
+++ lib/widgets/RecipeBookAppBar.dart
@@ -17,20 +17,9 @@ class RecipeBookAppBar extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           title: const Text('AppBar Sample'),
-          // This check specifies which nested Scrollable's scroll notification
-          // should be listened to.
-          //
-          // When `ThemeData.useMaterial3` is true and scroll view has
-          // scrolled underneath the app bar, this updates the app bar
-          // background color and elevation.
-          //
-          // This sets `notification.depth == 1` to listen to the scroll
-          // notification from the nested `ListView.builder`.
           notificationPredicate: (ScrollNotification notification) {
             return notification.depth == 1;
           },
-          // The elevation value of the app bar when scroll view has
-          // scrolled underneath the app bar.
           scrolledUnderElevation: 4.0,
           shadowColor: Theme.of(context).shadowColor,
           bottom: TabBar(
