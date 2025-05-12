5bff564dd61e368dc2f98f4fbc4a2199f90bee49
Nipun Tharuka Fernando <nipunjayanga@gmail.com>
2025-05-12 03:03:18 -0400

    changed dockerfile

--- express_server/index.mjs
+++ express_server/index.mjs
@@ -12,7 +12,7 @@ import { JSDOM } from 'jsdom';
 const app = Express();
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
   limit: 40,
